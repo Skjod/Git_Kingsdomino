362b72bf957e8afa541656895ed2d9c5bc298ae0
Skjod <lauratanddok@gmail.com>
2025-09-24 10:38:36 +0200

    testkode i Main

---
+++ Main.py
@@ -0,0 +1,23 @@
+import cv2 as cv
+import numpy as np
+
+img = cv.imread("ImageFiles/CroppedBoards/1.jpg")
+template = cv.imread("ImageFiles/Templates/Krone.png")
+
+hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+
+templated = cv.matchTemplate(img, template, cv.TM_CCOEFF_NORMED)
+
+''''mask = cv.inRange(hsv, (36, 25, 25), (70, 255,255))
+
+imask = mask>0
+green = np.zeros_like(img, np.uint8)
+green[imask] = img[imask]
+
+cv.imwrite("green.png", green)
+
+cv.imshow("green img", green)
+cv.waitKey(0)'''
+
+cv.imshow("templated img", templated)
+cv.waitKey(0)
