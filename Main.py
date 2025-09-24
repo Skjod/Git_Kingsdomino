ddc5ede1a3e7461f007283e9703719e9ed82a465
Skjod <lauratanddok@gmail.com>
2025-09-24 10:54:33 +0200

    paul test

--- Main.py
+++ Main.py
@@ -6,9 +6,9 @@ template = cv.imread("ImageFiles/Templates/Krone.png")
 
 hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
 
-templated = cv.matchTemplate(img, template, cv.TM_CCOEFF_NORMED)
+#templated = cv.matchTemplate(img, template, cv.TM_CCOEFF_NORMED)
 
-''''mask = cv.inRange(hsv, (36, 25, 25), (70, 255,255))
+mask = cv.inRange(hsv, (36, 25, 25), (70, 255,255))
 
 imask = mask>0
 green = np.zeros_like(img, np.uint8)
@@ -16,8 +16,6 @@ green[imask] = img[imask]
 
 cv.imwrite("green.png", green)
 
-cv.imshow("green img", green)
-cv.waitKey(0)'''
 
-cv.imshow("templated img", templated)
+cv.imshow("green img", green)
 cv.waitKey(0)
