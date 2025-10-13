49f905bca42757b2077b5720d00aa617f325c0e5
paragoncode <54541723+paragoncode@users.noreply.github.com>
2025-10-13 13:57:27 +0200

    match template test (virker ikke)

--- blobTest.py
+++ blobTest.py
@@ -2,8 +2,12 @@ import cv2
 import numpy as np
 
 # Load image
-image = cv2.imread("ImageFiles/CroppedBoards/3.jpg", cv2.IMREAD_GRAYSCALE)
-thresh = cv2.threshold(image,127,255,cv2.THRESH_BINARY)[1]
+imageMatch = cv2.imread("ImageFiles/CroppedBoards/1.jpg")
+img = cv2.imread("ImageFiles/CroppedBoards/1.jpg", cv2.IMREAD_GRAYSCALE)
+temp = cv2.imread("ImageFiles/crown3.png")
+match = cv2.matchTemplate(imageMatch, temp, cv2.TM_CCOEFF_NORMED)
+threshTemp = cv2.threshold(match, 0.55, 255, cv2.THRESH_BINARY)[1]
+thresh = cv2.threshold(img,127,255,cv2.THRESH_BINARY)[1]
 
 # Setup SimpleBlobDetector parameters
 params = cv2.SimpleBlobDetector_Params()
@@ -24,11 +28,11 @@ params.maxArea = 400
 # Filter by Circularity
 params.filterByCircularity = True
 params.minCircularity = 0.1
-params.maxCircularity = 1.5
+params.maxCircularity = 10
 
 # Filter by Convexity
-params.filterByConvexity = False
-params.minConvexity = 0.8
+params.filterByConvexity = True
+params.minConvexity = 0.3
 
 # Filter by Inertia
 params.filterByInertia = False
@@ -45,4 +49,7 @@ output = cv2.drawKeypoints(thresh, keypoints, np.array([]), (0, 0, 255), cv2.DRA
 
 # Show the output
 cv2.imshow("Blobs Detected", output)
+cv2.imshow("image", imageMatch)
+cv2.imshow("match", match)
+cv2.imshow("thresh", threshTemp)
 cv2.waitKey(0)
