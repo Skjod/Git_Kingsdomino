f7fc3a0bf917f348e037196c533710b42683e397
paragoncode <54541723+paragoncode@users.noreply.github.com>
2025-10-15 14:17:48 +0200

    blobtest update

--- blobTest.py
+++ blobTest.py
@@ -2,11 +2,20 @@ import cv2
 import numpy as np
 
 # Load image
-imageMatch = cv2.imread("ImageFiles/CroppedBoards/1.jpg")
-img = cv2.imread("ImageFiles/CroppedBoards/1.jpg", cv2.IMREAD_GRAYSCALE)
+imageMatch = cv2.imread("ImageFiles/CroppedBoards/2.jpg")
+img = cv2.imread("ImageFiles/CroppedBoards/2.jpg", cv2.IMREAD_GRAYSCALE)
 temp = cv2.imread("ImageFiles/crown3.png")
+temp90 = cv2.imread("ImageFiles/crown90.png")
+temp180 = cv2.imread("ImageFiles/crown180.png")
+temp270 = cv2.imread("ImageFiles/crown270.png")
 match = cv2.matchTemplate(imageMatch, temp, cv2.TM_CCOEFF_NORMED)
+match90 = cv2.matchTemplate(imageMatch, temp90, cv2.TM_CCOEFF_NORMED)
+match180 = cv2.matchTemplate(imageMatch, temp180, cv2.TM_CCOEFF_NORMED)
+match270 = cv2.matchTemplate(imageMatch, temp270, cv2.TM_CCOEFF_NORMED)
 threshTemp = cv2.threshold(match, 0.55, 255, cv2.THRESH_BINARY)[1]
+threshTemp90 = cv2.threshold(match90, 0.55, 255, cv2.THRESH_BINARY)[1]
+threshTemp180 = cv2.threshold(match180, 0.55, 255, cv2.THRESH_BINARY)[1]
+threshTemp270 = cv2.threshold(match270, 0.55, 255, cv2.THRESH_BINARY)[1]
 thresh = cv2.threshold(img,127,255,cv2.THRESH_BINARY)[1]
 
 # Setup SimpleBlobDetector parameters
@@ -52,4 +61,7 @@ cv2.imshow("Blobs Detected", output)
 cv2.imshow("image", imageMatch)
 cv2.imshow("match", match)
 cv2.imshow("thresh", threshTemp)
+cv2.imshow("thresh90", threshTemp90)
+cv2.imshow("thresh180", threshTemp180)
+cv2.imshow("thresh270", threshTemp270)
 cv2.waitKey(0)
