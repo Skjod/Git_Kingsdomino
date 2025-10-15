f01acc752a151a4431446fd0565566091feeafbd
pskrub <paulskrubbeltrang@gmail.com>
2025-10-15 13:40:34 +0200

    testing stuff don't use this it don't work

--- MatrixTest.py
+++ MatrixTest.py
@@ -2,37 +2,34 @@ import numpy as np
 import cv2 as cv
 from collections import deque
 
+### create example matrix
 rows = 5
 cols = 5
-
 tileElements = ["forest", "grass", "water", "desert"]
 plane = []
-
-
 for i in range(rows):
     row = []
     for j in range(cols):
         row.append(tileElements[0])
     plane.append(row)
-
 plane[1][1] = tileElements[2]
 plane[1][2] = tileElements[2]
 plane[3][3] = tileElements[2]
 # match = "water"
-noOfMatches = 0
 
+### spit function
 dq = deque([])
 # blob = [] #information stored in current blob
 # blobs = [] #collected information about blobs
 
-
 def spit(i, j, matrix, blobNum, blob, blobs, match):
     blob += 1
+    print(i, ", ", j, matrix[i][j])
     if dq:
         dq.popleft()
         dq.popleft()
 
-    if matrix[i][j+1] == match:
+    if matrix[i][j+1] != None and matrix[i][j+1] == match:
         dq.append(i)
         dq.append(j+1)
     if matrix[i+1][j] == match:
@@ -45,8 +42,9 @@ def spit(i, j, matrix, blobNum, blob, blobs, match):
         dq.append(i)
         dq.append(j-1)
 
-    matrix[i][j] = "burnt"
-
+    # if dq[1] == "burnt":
+    #     while "burnt" in dq:
+    #         deque.remove("burnt")
     if dq:
         spit(dq[0], dq[1], matrix, blobNum, blob, blobs, match)
     else:
@@ -64,10 +62,10 @@ def seperateTiles(matrix, tileTypes):
         for i in range(rows):
             for j in range(cols):
                 if matrix[i][j] == match:
-                    spit(i, j, matrix, blobNum, blob, blobs, match)
-                    blobNum += 1
                     print("blob detected")
                     print(i, j)
+                    spit(i, j, matrix, blobNum, blob, blobs, match)
+                    blobNum += 1
     print(blobs)
 
 
