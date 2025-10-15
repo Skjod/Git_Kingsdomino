d897964eccbc590c9f3b99495140d75222b22f80
pskrub <paulskrubbeltrang@gmail.com>
2025-10-15 13:00:51 +0200

    tilføjede arguments til pointtæller metoden

--- MatrixTest.py
+++ MatrixTest.py
@@ -5,67 +5,75 @@ from collections import deque
 rows = 5
 cols = 5
 
-tileType = ["forest", "grass", "water", "desert"]
+tileElements = ["forest", "grass", "water", "desert"]
 plane = []
 
 
 for i in range(rows):
     row = []
     for j in range(cols):
-        row.append(tileType[0])
+        row.append(tileElements[0])
     plane.append(row)
 
-plane[1][1] = tileType[2]
-plane[1][2] = tileType[2]
-plane[3][3] = tileType[2]
-match = "water"
+plane[1][1] = tileElements[2]
+plane[1][2] = tileElements[2]
+plane[3][3] = tileElements[2]
+# match = "water"
 noOfMatches = 0
 
 dq = deque([])
-blob = []
-blobs = []
+# blob = [] #information stored in current blob
+# blobs = [] #collected information about blobs
 
 
-def spit(i, j, blobNum):
-    blob.append(i)
-    blob.append(j)
+def spit(i, j, matrix, blobNum, blob, blobs, match):
+    blob += 1
     if dq:
         dq.popleft()
         dq.popleft()
 
-    if plane[i][j+1] == match:
+    if matrix[i][j+1] == match:
         dq.append(i)
         dq.append(j+1)
-    if plane[i+1][j] == match:
+    if matrix[i+1][j] == match:
         dq.append(i+1)
         dq.append(j)
-    if plane[i-1][j] == match:
+    if matrix[i-1][j] == match:
         dq.append(i-1)
         dq.append(j)
-    if plane[i][j-1] == match:
+    if matrix[i][j-1] == match:
         dq.append(i)
         dq.append(j-1)
 
-    plane[i][j] = "burnt"
+    matrix[i][j] = "burnt"
+
     if dq:
-        spit(dq[0], dq[1], blobNum)
+        spit(dq[0], dq[1], matrix, blobNum, blob, blobs, match)
     else:
-        blobs.append(blob.copy())
-        blob.clear()
+        blobs.append(blob)
 
 
-# for i in range(rows):
-#     for j in range(cols):
-#         print(plane[i][j], end=" ")
-#     print()
+def seperateTiles(matrix, tileTypes):
+    blobNum = 0
+    blob = 0
+    blobs = []
+    match = ""
+    for type in tileTypes:
+        match = type
+        print(match)
+        for i in range(rows):
+            for j in range(cols):
+                if matrix[i][j] == match:
+                    spit(i, j, matrix, blobNum, blob, blobs, match)
+                    blobNum += 1
+                    print("blob detected")
+                    print(i, j)
+    print(blobs)
 
 
 for i in range(rows):
     for j in range(cols):
-        if plane[i][j] == match:
-            spit(i, j, noOfMatches)
-            noOfMatches += 1
-            print("blob detected")
-            print(i, j)
+        print(plane[i][j], end=" ")
+    print()
 
-print(blobs)
+seperateTiles(plane, tileElements)
