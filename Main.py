fe406435f97f51806429c6fd3a58a1db5dff6c5b
pskrub <paulskrubbeltrang@gmail.com>
2025-10-24 09:28:09 +0200

    renamed matrixTest to PointCounter

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
+from PointCounter import seperateTiles
 
 #importer billede
 img = cv.imread("ImageFiles/CroppedBoards/56.jpg")
