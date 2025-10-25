f0dcecd5e751e78f1dea09f91883e8c16eec57ea
pskrub <paulskrubbeltrang@gmail.com>
2025-10-25 15:47:53 +0200

    byttede rundt på i og j så førstekoordinatet er vandret

--- PointCounter.py
+++ PointCounter.py
@@ -72,15 +72,15 @@ def PointCounter_fun(matrix,crowns,tileTypes):
                 if matrix[i][j] == match:
                     #print("blob detected")
                     spit(i, j, matrix, blobNum, blob, crownCount, match, crowns)
-                    coordinates += str(match) + " at: (" + str(i + 1) + ", " + str(j + 1) + "), with " + str(blobs[blobNum]) + " connected tiles\n"
+                    coordinates += str(match) + " at: (" + str(j + 1) + ", " + str(i + 1) + "), with " + str(blobs[blobNum]) + " connected tiles\n"
                     blobNum += 1
 
     for i in range(rows):
         for j in range(cols):
             if crowns[i][j] > 0:
-                coordinates += "\n" + str(crowns[i][j]) + " crown(s) detected at: (" + str(i + 1) + ", " + str(j + 1) + ")"
+                coordinates += "\n" + str(crowns[i][j]) + " crown(s) detected at: (" + str(j + 1) + ", " + str(i + 1) + ")"
     coordinates += "\n\ntotal points: " + str(sum(points))
 
     return coordinates
 
-print(PointCounter_fun(plane, planeCrowns, tileElements))
+# print(PointCounter_fun(plane, planeCrowns, tileElements))
