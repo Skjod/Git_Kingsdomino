fd87f9ebe175711b783ad302d2a2c1f94d8efd14
pskrub <paulskrubbeltrang@gmail.com>
2025-10-25 15:10:06 +0200

    lidt kode cleanup

--- PointCounter.py
+++ PointCounter.py
@@ -31,6 +31,7 @@ def PointCounter_fun(matrix,crowns,tileTypes):
     dq = deque([])
     blob = 0 #information stored in current blob
     blobs = [] #collected information about blobs
+    blobNum = 0
     points = []
     crownCount = 0
     coordinates = ""
@@ -64,7 +65,6 @@ def PointCounter_fun(matrix,crowns,tileTypes):
             points.append(blobs[blobNum] * crownCount)
 
 
-    blobNum = 0
     for match in tileTypes:
         #print(match)
         for i in range(rows):
@@ -75,21 +75,12 @@ def PointCounter_fun(matrix,crowns,tileTypes):
                     coordinates += str(match) + " at: (" + str(i + 1) + ", " + str(j + 1) + "), with " + str(blobs[blobNum]) + " connected tiles\n"
                     blobNum += 1
 
-    coordinates += "\ncrowns detected at: "
     for i in range(rows):
         for j in range(cols):
             if crowns[i][j] > 0:
                 coordinates += "\n" + str(crowns[i][j]) + " crown(s) detected at: (" + str(i + 1) + ", " + str(j + 1) + ")"
     coordinates += "\n\ntotal points: " + str(sum(points))
 
-    #print(points)
     return coordinates
-
-    '''for i in range(rows):
-        for j in range(cols):
-            print(plane[i][j], end=" ")
-        print()'''
-
-    seperateTiles(plane, planeCrowns, tileElements)
 
 print(PointCounter_fun(plane, planeCrowns, tileElements))
