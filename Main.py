905cbfb5aab917b2b86ee8a8f932b8aa24008cb2
Skjod <lauratanddok@gmail.com>
2025-10-27 16:10:58 +0100

    Sidste små detaljer

--- Main.py
+++ Main.py
@@ -4,7 +4,6 @@ from Classifier import Classifier_fun
 from CrownDetection import Crown_fun
 
 
-
 image = cv.imread("ImageFiles/Test data/73.jpg")
 
 labels = ["forest", "grasslands", "wheat", "swamp", "mine", "lake"]
@@ -14,36 +13,3 @@ tiles = Classifier_fun(image)
 points = PointCounter_fun(tiles, crowns, labels)
 
 print(points)
-#print(Classifier_method(image))
-
-'''import cv2 as cv
-import numpy as np
-import os
-from PointCounterLaura import PointCounter_fun_laura
-from Classifier import Classifier_fun
-from CrownDetection import Crown_fun
-
-# Mappen med billeder
-image_folder = "ImageFiles/Test data"
-
-# Tile labels
-labels = ["forest", "grasslands", "wheat", "swamp", "mine", "lake"]
-
-# Loop over alle billeder
-for idx, file in enumerate(os.listdir(image_folder)):
-    full_path = os.path.join(image_folder, file)
-    image = cv.imread(full_path)
-    if image is None:
-        print(f"Could not load image: {full_path}")
-        continue
-
-    print(f"\nProcessing board {idx}: {file}")
-
-    # Kør klassificering og crowns detection
-    tiles = Classifier_fun(image)
-    crowns = Crown_fun(image)
-
-    # Kør point counter og gem tiles til confusion matrix
-    points = PointCounter_fun_laura(tiles, crowns, labels, board_idx=idx)
-
-    print(f"Board {idx} ({file}) score: {points}")'''
